ed407a68da4855975d2d7fc3501f393db96f7aac
ijefin <jeffersonmecmiler@hotmail.com>
2023-04-05 20:43:15 -0300

    added buttons

--- frontend/src/components/Cards/TaskCard.tsx
+++ frontend/src/components/Cards/TaskCard.tsx
@@ -1,16 +1,35 @@
-import React from 'react'
 import "./TaskCard.css"
+import { useState, useEffect } from "react"
+import { useFetch } from "../../hooks/useFetch"
+
 
 export const TaskCard = () => {
+
+    const url = "http://localhost:3000/all-tasks";
+
+    const { data: tasks, loading, error } = useFetch(url)
+
+    console.log(tasks)
+
     return (
         <>
             <div className="task-card">
                 <div className="task-title">
-                    <p>Lavar Roupa</p>
+                    <h1>Limpar o quarto</h1>
                 </div>
                 <div className="card-body">
-                    <p>Lavar as roupas pendentes que estão na cama</p>
+                    <p>Limpar a o quarto e guardar as roupas</p>
+                    <div className="dropdown">
+                        <button id="test" className="dropbtn"></button>
+                        <div className="dropdown-content">
+                            <button >Apagar <i id='delete' className="fa-solid fa-trash"></i></button>
+                            <button >Editar <i id='edit' className="fa-solid fa-edit"></i></button>
+                            <button >Concluído <i id='done' className="fa-solid fa-check"></i></button>
+                        </div>
+
+                    </div>
                 </div>
+
             </div>
         </>
     )
---
+++ frontend/src/hooks/useFetch.tsx
@@ -0,0 +1,93 @@
+import { useState, useEffect } from "react"
+
+// custom hook
+export const useFetch = (url: string) => {
+
+    const [data, setData] = useState([])
+    const [config, setConfig]: any = useState(null)
+    const [method, setMethod]: any = useState(null)
+    const [callFetch, setCallFetch] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState("")
+    const [itemId, setItemId] = useState()
+
+    const httpConfig = (data: any, method: string) => {
+        if (method === "POST") {
+            setConfig({
+                method,
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+
+            setMethod(method)
+        }
+
+        if (method === "DELETE") {
+            setConfig({
+                method,
+                headers: {
+                    "Content-type": "application/json"
+                }
+            })
+
+            setMethod(method)
+            setItemId(data)
+        }
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+
+            setLoading(true)
+
+            try {
+                const res = await fetch(url)
+
+                const json = await res.json()
+                setData(json)
+
+            } catch (error) {
+                console.log(error)
+                setError("Houve um erro ao carregar os dados")
+            }
+
+            setLoading(false)
+        }
+
+        fetchData()
+    }, [url, callFetch])
+
+    useEffect(() => {
+        const httpRequest = async () => {
+            let json
+
+            if (method === "POST") {
+                let fetchOptions = [url, config] as const
+
+                const res = await fetch(...fetchOptions)
+
+                json = await res.json()
+
+            }
+
+            if (method === "DELETE") {
+                const res = await fetch(`${url}/${itemId}`, config)
+                json = await res.json()
+
+                setCallFetch(json)
+            }
+
+            setCallFetch(json)
+
+        }
+
+
+
+        httpRequest()
+    }, [config, method, url])
+
+    return { data, httpConfig, loading, error }
+
+}
