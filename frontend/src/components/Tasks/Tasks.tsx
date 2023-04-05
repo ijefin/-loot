6709f14ff198cc0cb8d1b68e8c35ed6ddc619db8
ijefin <jeffersonmecmiler@hotmail.com>
2023-04-05 17:57:06 -0300

    added home view

---
+++ frontend/src/components/Tasks/Tasks.tsx
@@ -0,0 +1,19 @@
+
+export const Tasks = () => {
+    return (
+        <>
+            <div className="title">
+                <h1>Tarefas</h1>
+            </div>
+            <div className="task-container">
+                <div className="done-tasks">
+
+                </div>
+                <div className="not-done-tasks">
+
+                </div>
+            </div>
+        </>
+
+    )
+}
--- frontend/src/views/Home/Home.tsx
+++ frontend/src/views/Home/Home.tsx
@@ -1,17 +1,25 @@
 import "./Home.css"
+import home from "../../assets/home.png"
+import { Tasks } from "../../components/Tasks/Tasks"
 
 export const Home = () => {
     return (
         <>
             <div className="main-home-container">
-                <div className="title">
-                    <h1>Quebre seus afazeres <br /> em pequenas tarefas <br /> para uma melhor  <br /> organização.</h1>
+                <div className="main-content">
+                    <div className="title">
+                        <h1>Quebre seus afazeres <br /> em pequenas tarefas <br /> para obter mais <br /> produtividade.</h1>
+                    </div >
+                    <div className="add-new-task">
+                        <p>O costume de organizar melhor seu dia a dia <br /> baseado em tarefas pode deixar sua rotina <br />   mais agradável e mais produtiva</p>
+                        <button className="add-new"><span className="button-text">Começar</span></button>
+                    </div>
                 </div>
-                <div className="add-new-task">
-                    <p>O costume de organizar melhor seu dia-a-dia baseado em tarefas pode deixar sua rotina <br />   mais agradável e mais produtiva</p>
-                    <button className="add-new">Nova Tarefa +</button>
+                <div className="image-container">
+                    <img src={home} />
                 </div>
             </div >
+            <hr />
         </>
     )
 }
