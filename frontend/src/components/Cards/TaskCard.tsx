42c37aa31acff9174d9ca82650690e1b7df1e05a
ijefin <jeffersonmecmiler@hotmail.com>
2023-04-06 21:17:21 -0300

    added new options

--- frontend/src/components/Cards/TaskCard.tsx
+++ frontend/src/components/Cards/TaskCard.tsx
@@ -1,5 +1,6 @@
 import "./TaskCard.css"
 import { useState, useEffect } from "react"
+import { useFetch } from "../../hooks/useFetch"
 
 interface Task {
     name: string
@@ -7,30 +8,33 @@ interface Task {
     done?: boolean
 }
 
+
 export const TaskCard = ({ name, description, done }: Task) => {
 
     return (
         <>
             {
-                <div className="task-card">
-                    <div className="task-title">
-                        <h2>{name}</h2>
-                        <div className="dropdown">
-                            <button id="dots" className="dropbtn"></button>
-                            <div className="dropdown-content">
-                                <button >Concluir <i id='done' className="fa-solid fa-check"></i></button>
-                                <button >Editar <i id='edit' className="fa-solid fa-edit"></i></button>
-                                <button >Apagar <i id='delete' className="fa-solid fa-trash"></i></button>
-                            </div>
-                        </div>
+                <div className="two">
+                    <div className="d-flex justify-content-end px-3 pt-1"><i className="mdi mdi-star-outline pr-1 star"></i><i className="mdi mdi-dots-horizontal dot"></i></div>
+                    <div className="px-3 pt-3">
+                        <h3 className="name">{name}</h3>
+                        <p className="quote2">{description}</p>
                     </div>
-                    <div className="card-body">
-                        <div className="card-description">
-                            <p>{description}</p>
+                    <div className="d-flex justify-content-start px-3 align-items-center">
+                        <i className="mdi mdi-view-comfy task"></i>
+                    </div>
+                    <div className="d-flex justify-content-between  px-3 align-items-center pb-3">
+                        <div className="d-flex justify-content-start align-items-center">
                         </div>
-                        <div className="card-checkbox">
-                            <p>Marcar como concluído</p>
-                            <input type="checkbox" name="mark-as-done" id="done" />
+                        <div className="d-flex justify-content-end">
+                            <div className="dropdown">
+                                <button id="dots" className="dropbtn"></button>
+                                <div className="dropdown-content">
+                                    <button >Concluir <i id='done' className="fa-solid fa-check"></i></button>
+                                    <button >Editar <i id='edit' className="fa-solid fa-edit"></i></button>
+                                    <button >Apagar <i id='delete' className="fa-solid fa-trash"></i></button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
--- frontend/src/components/Tasks/Tasks.tsx
+++ frontend/src/components/Tasks/Tasks.tsx
@@ -20,15 +20,42 @@ export const Tasks = () => {
 
     return (
         <>
-            <div className="container">
-                <div className="top-content">
-                    <div className="title">
-                        <h3>Organize suas tarefas</h3>
-                    </div>
-                    <div className="new-task">
-                        <button>Nova</button>
+
+            <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+                Launch demo modal
+            </button>
+            <div className="modal fade" id="exampleModalCenter" tabIndex={1} role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            ...
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Save changes</button>
+                        </div>
                     </div>
                 </div>
+            </div>
+
+
+            <div className="box">
+                <div className="one">
+                    <div className="px-3 pb-4">
+                        <div><img src="https://img.icons8.com/bubbles/50/000000/blond-short-hair-lady-with-blue-glasses.png" width="15" className="pic1" /><img src="https://img.icons8.com/bubbles/50/000000/girl-with-chemical-test-tube.png" width="22" className="pic2" /></div>
+                        <div><img src="https://img.icons8.com/bubbles/100/000000/girl-with-glasses-art-palette.png" width="65" /></div>
+                        <div><img src="https://img.icons8.com/bubbles/50/000000/short-curly-hair-girl-gem.png" width="16" className="pic3" /><img src="https://img.icons8.com/bubbles/50/000000/girl-and-playing-card.png" width="16" className="pic4" /></div>
+                        <div><p className="quote">Clique para criar uma nova tarefa</p></div>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
                 <div className="task-container">
                     <div className="task-status">
                         <h3>Em progresso</h3>
